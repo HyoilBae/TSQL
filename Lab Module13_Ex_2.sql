74be6e37c36c48a624eafaa4921a5d25900f896a
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-22 10:11:20 -0800

    Create Lab Module13_Ex_2.sql

---
+++ Lab Module13_Ex_2.sql
@@ -0,0 +1,26 @@
+WITH OrderRows AS
+(
+SELECT orderid, orderdate, ROW_NUMBER() OVER (ORDER BY orderdate, orderid) AS rowno, val
+FROM Sales.OrderValues
+)
+SELECT o.orderid, o.orderdate, o.val, o2.val as prevval, o.val - o2.val as diffprev
+FROM OrderRows AS o
+LEFT OUTER JOIN OrderRows AS o2 ON o.rowno = o2.rowno + 1;
+
+SELECT orderid, orderdate, val, LAG(val) OVER (ORDER BY orderdate, orderid) AS prevval,
+val - LAG(val) OVER (ORDER BY orderdate, orderid) AS diffprev
+FROM Sales.OrderValues;
+
+WITH SalesMonth2007 AS
+(
+SELECT MONTH(orderdate) AS monthno, SUM(val) AS val
+FROM Sales.OrderValues
+WHERE orderdate >= '20070101' AND orderdate < '20080101'
+GROUP BY MONTH(orderdate)
+)
+SELECT monthno, val,
+(LAG(val, 1, 0) OVER (ORDER BY monthno) + LAG(val, 2, 0) OVER (ORDER BY monthno) +
+LAG(val, 3, 0) OVER (ORDER BY monthno)) / 3 AS avglast3months,
+val - FIRST_VALUE(val) OVER (ORDER BY monthno ROWS UNBOUNDED PRECEDING) AS diffjanuary,
+LEAD(val) OVER (ORDER BY monthno) AS nextval
+FROM SalesMonth2007;
